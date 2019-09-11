bf336d1299e623bab8dcfab0104aeb289cb133ca
Anon NIPS <anonipous@gmail.com>
2019-09-11 18:47:00 +0200

    Initial commit

---
+++ gym/spaces/discrete_extended.py
@@ -0,0 +1,14 @@
+import numpy as np
+import gym
+from gym.spaces import Discrete, Box
+
+class DiscreteExtended(Discrete):
+
+    def __init__(self, n, seed=None):
+        super(DiscreteExtended, self).__init__(n, seed=seed)
+
+    def sample(self, prob=None, size=1, replace=True):
+        sampled = np.squeeze(self.np_random.choice(self.n, size=size, p=prob, replace=replace))
+        if sampled.shape == ():
+            sampled = int(sampled) #TODO Just made it an int otherwise np.array(scalar) looks ugly in output.
+        return sampled
